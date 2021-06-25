7c0b940fde91441413b92cf1867b206cd0ebe029
Nikita C <nikita149156@gmail.com>
2021-06-25 11:00:14 +0530

    show pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root "movies#index"
+  # resources :movies
   get "/movies", to: "movies#index"
-  get "/movies/:id", to: "movies#index"
+  get "/movies/:id", to: "movies#show", as: 'movie'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
